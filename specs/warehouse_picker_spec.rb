2d8e4abbfb1d483666aea6119583696857dd051b
StevenMeiklejohn <stevenmeiklejohn700@gmail.com>
2016-04-29 11:02:16 +0100

    Homwork_Initial_Commmit" gst pwd pwd quit q "

---
+++ specs/warehouse_picker_spec.rb
@@ -0,0 +1,19 @@
+require ('minitest/autorun')
+require_relative '../warehouse_picker_functions.rb'
+
+class TestWarehouse < Minitest::Test
+
+
+#1
+
+def test_item_at_bay
+  item = item_at_bay('b5')
+  assert_equal('nail filer', item)
+end
+
+
+
+
+
+
+end
